fc6ff77eaa4b76165d5db78ae236b02fcfec6f5e
Kseniya Radiseva <Kotaviy@gmail.com>
2023-08-31 22:11:57 +0500

    Changes was made

--- src/test/java/ru/netology/CardRequestTest.java
+++ src/test/java/ru/netology/CardRequestTest.java
@@ -1,5 +1,7 @@
 package ru.netology;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -15,6 +17,6 @@ public class CardRequestTest {
         form.$("[data-test-id=phone] input").setValue("+79990000000");
         form.$("[data-test-id=agreement]").click();
         form.$(By.cssSelector(".button_size_m")).click();
-        $("order-success");
+        $("[data-test-id=order-success]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
